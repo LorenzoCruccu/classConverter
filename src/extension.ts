fa499e6ddc03992561dc7b4742641faffbf8ce45
Lorenzo <lorenzo.cruccu@elbuild.it>
2021-08-02 09:43:03 +0200

    removed generated hello world

--- src/extension.ts
+++ src/extension.ts
@@ -10,15 +10,6 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "classConverter" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('classConverter.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from classConverter!');
-	});
-
 	let be2fe = vscode.commands.registerCommand('classConverter.be2fe', () => {
 		
 		// Get the active text editor
